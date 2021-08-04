f721fcb65672cbed744dc408d968b5323c2c4ade
KarolisGaiv <kgaivenis@gmail.com>
2021-08-04 15:58:52 +0300

    Finish refactoring personal details form and fix state passing between parent and component

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,33 @@
-import React, { useState } from "react";
-import PersonalDetailsForm from "./components/CVForm/PersonalDetailsForm/PersonalDetails";
+import React, { useState } from 'react';
+import PersonalDetailsForm from './components/CVForm/PersonalDetailsForm/PersonalDetails';
 // import PersonDetails from "./components/CVPreview/PersonDetails/PersonDetails";
-import "./styles/styles.scss";
-
-// function App() {
-//   const [name, setName] = useState("");
-//   const [surname, setSurname] = useState("");
-//   const [email, setEmail] = useState("");
-//   const [number, setNumber] = useState("");
-//   const [person, setPerson] = useState({});
-
-//   function handleName(e) {
-//     setName(e.target.value);
-//   }
-//   function handleSurname(e) {
-//     setSurname(e.target.value);
-//   }
-//   function handleEmail(e) {
-//     setEmail(e.target.value);
-//   }
-//   function handleNumber(e) {
-//     setNumber(e.target.value);
-//   }
-
-//   function handleSubmit(e) {
-//     e.preventDefault();
-//     setPerson(name, surname, email, number);
-//     console.log(person);
-//   }
-
-//   return (
-//     <div className="content-wrapper">
-//       <PersonalDetailsForm
-//         name={name}
-//         surname={surname}
-//         email={email}
-//         number={number}
-//         handleName={handleName}
-//         handleSurname={handleSurname}
-//         handleEmail={handleEmail}
-//         handleNumber={handleNumber}
-//         handleSubmit={handleSubmit}
-//       />
-//       <PersonDetails person={person} />
-//     </div>
-//   );
-// }
+import './styles/styles.scss';
 
 function App() {
-  const [name, setName] = useState("")
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+  const [email, setEmail] = useState('');
+  const [number, setNumber] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(name, surname, email, number);
+  }
+
   return (
     <div>
-      <h1>{name}</h1>
-      <PersonalDetailsForm setName={setName} />
+      <h1>
+        {name} and {surname}
+      </h1>
+      <PersonalDetailsForm
+        setName={setName}
+        setSurname={setSurname}
+        setEmail={setEmail}
+        setNumber={setNumber}
+        handleSubmit={handleSubmit}
+      />
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/CVForm/PersonalDetailsForm/PersonalDetails.jsx
+++ src/components/CVForm/PersonalDetailsForm/PersonalDetails.jsx
@@ -1,89 +1,64 @@
-import React from "react";
-import "./personalDetails.scss";
+import React from 'react';
+import './personalDetails.scss';
 
-// function PersonalDetailsForm({
-//   name,
-//   surname,
-//   email,
-//   number,
-//   handleName,
-//   handleSurname,
-//   handleEmail,
-//   handleNumber,
-//   handleSubmit,
-// }) {
-//   // const [name, setName] = useState("");
-//   // const [surname, setSurname] = useState("");
-//   // const [email, setEmail] = useState("");
-//   // const [number, setNumber] = useState("");
-
-//   // function handleSubmit(e) {
-//   //   e.preventDefault();
-//   // }
-
-//   return (
-//     <div className="details-form-wrapper">
-//       <h1>Personal Details</h1>
-//       <form className="details-form" onSubmit={handleSubmit}>
-//         <label htmlFor="name-field">Name</label>
-//         <input
-//           type="text"
-//           id="name-field"
-//           placeholder="John"
-//           value={name}
-//           onChange={handleName}
-//         />
-//         <label htmlFor="surname-field">Last Name</label>
-//         <input
-//           type="text"
-//           id="surname-field"
-//           placeholder="Doe"
-//           value={surname}
-//           onChange={handleSurname}
-//         />
-//         <label htmlFor="email-field">Email</label>
-//         <input
-//           type="email-field"
-//           id="email-field"
-//           placeholder="example@gmail.com"
-//           value={email}
-//           onChange={handleEmail}
-//         />
-//         <label htmlFor="phone-field">Phone Number</label>
-//         <input
-//           type="number"
-//           id="phone-field"
-//           value={number}
-//           onChange={handleNumber}
-//         />
-//         <input type="submit" value="Submit"></input>
-//       </form>
-//     </div>
-//   );
-// }
-
-function PersonalDetailsForm({ setName }) {
-
+function PersonalDetailsForm({
+  setName,
+  setSurname,
+  setEmail,
+  setNumber,
+  handleSubmit,
+}) {
   function handleName(e) {
-    const name = e.target.value
-    setName(name)
+    const name = e.target.value;
+    setName(name);
   }
 
-  return(
-    <div className="details-form-wrapper">
+  function handleSurname(e) {
+    const surname = e.target.value;
+    setSurname(surname);
+  }
+
+  function handleEmail(e) {
+    const email = e.target.value;
+    setEmail(email);
+  }
+
+  function handleNumber(e) {
+    const number = e.target.value;
+    setNumber(number);
+  }
+
+  return (
+    <div className='details-form-wrapper'>
       <h1>Personal Details</h1>
-      <form className="details-form">
-      <label htmlFor="name-field">Name</label>
-         <input
-          type="text"
-          id="name-field"
-          placeholder="John"
+      <form className='details-form' onSubmit={handleSubmit}>
+        <label htmlFor='name-field'>Name</label>
+        <input
+          type='text'
+          id='name-field'
+          placeholder='John'
           onChange={handleName}
         />
-        <button type="submit">Add</button>
+        <label htmlFor='surname-field'>Last Name</label>
+        <input
+          type='text'
+          id='surname-field'
+          placeholder='Doe'
+          onChange={handleSurname}
+        />
+        <label htmlFor='email-field'>Email</label>
+        <input
+          type='email-field'
+          id='email-field'
+          placeholder='example@gmail.com'
+          onChange={handleEmail}
+        />
+        <label htmlFor='phone-field'>Phone Number</label>
+        <input type='number' id='phone-field' onChange={handleNumber} />
+        <input type='submit' value='Submit'></input>
       </form>
     </div>
-  )
+  );
 }
 
 export default PersonalDetailsForm;
