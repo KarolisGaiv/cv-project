e4bf599aef33f62caa8e356ae785fa61522f460b
KarolisGaiv <kgaivenis@gmail.com>
2021-08-08 11:33:56 +0300

    Create basic education and work experience records in CV form view

--- src/components/CVPreview/UserCV/UserCV.jsx
+++ src/components/CVPreview/UserCV/UserCV.jsx
@@ -3,11 +3,36 @@ import React from 'react';
 function UserCV({ person, education, workExperience }) {
   return (
     <div className='cv-wrapper'>
-      <h1>{person.name + ' ' + person.surname}</h1>
-      {education.map((educationRecord) => (
-        <p>{educationRecord.school}</p>
-      ))}
-      {/* <p>{workExperience}</p> */}
+      <div className='personal-details-wrapper'>
+        <h2>
+          {person.name} {person.surname}
+        </h2>
+        <div className='personal-contacts-wrapper'>
+          <h4>{person.email}</h4>
+          <h4>{person.number}</h4>
+        </div>
+      </div>
+      <div className='personal-education-wrapper'>
+        {education.map((education) => (
+          <div className='education-period' key={education.study}>
+            {education.fromDate} - {education.toDate}
+            <div className='education-details'>
+              {education.study} {education.school}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className='work-experience-wrapper'>
+        {workExperience.map((workRecord) => (
+          <div className='work-period' key={workRecord.position}>
+            {workRecord.fromDate} {workRecord.toDate}
+            <div className='work-details'>
+              {workRecord.position} {workRecord.companyName}{' '}
+              {workRecord.jobDescription}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
